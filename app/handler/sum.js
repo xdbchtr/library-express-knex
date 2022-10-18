f7a55c60333562cf1eda2df52cb00924480071f8
adib.bachtiar <adib.bachtiar@mitra.jasamarga.co.id>
2022-10-18 08:55:00 +0700

    test

--- app/handler/sum.js
+++ app/handler/sum.js
@@ -1,11 +1,13 @@
 let CronJob = require('node-cron');
+const sendResponse = require("../resources/responseApi")
 
-async function test() {
+async function test(req, res) {
     const scheduledJobFunction = CronJob.schedule("* * * * *", () => {
         console.log("Cron is Running!");
     });
     
     scheduledJobFunction.start();
+    res.status(200).send(sendResponse.successGet(1))
 }
 
 module.exports = {
